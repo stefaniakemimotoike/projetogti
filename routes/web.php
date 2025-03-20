e7e1b4568953492c342b7843668c074b9eda05b5
stefaniakemimotoike <gureufubuki@gmail.com>
2025-03-20 10:57:59 -0300

    estudos sobre rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste', function () {
+    return "O codigo foi testado";
+});
+
+Route::get('/usuario/{nome}', function ($nome) {
+    return "o usuario atual é: " .$nome;
+});
+
+Route::get('/soma/{num1}/{num2}', function ($num1, $num2) {
+    return "a soma é: " .$num1 + $num2;
+});
+
+Route::get('/subtração/{num1}/{num2}', function ($num1, $num2) {
+    return "a subtração é: " .$num1 - $num2;
+});
+
+Route::get('/divisão/{num1}/{num2}', function ($num1, $num2) {
+    return "a divisão é: " .$num1 / $num2;
+});
+
+Route::get('/multiplicação/{num1}/{num2}', function ($num1, $num2) {
+    return "a multiplicação é: " .$num1 * $num2;
+});
